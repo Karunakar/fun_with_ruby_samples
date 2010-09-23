ad8e23cfa9451f645f1f8d12e9e0506c2df97aa7
Karunakar <revurikarna@gmail.com>
2010-09-23 23:02:31 +0530

    sample on define methods

---
+++ define_methods.rb
@@ -0,0 +1,21 @@
+def my_method
+	x = 10
+end
+
+x  = 
+5
+
+def define_methods
+	shared = 0
+ Kernel.send :define_method, :counter do
+	shared
+ end
+ Kernel.send :define_method, :inc do |x|
+	shared += x
+ end
+end
+define_methods
+puts counter
+puts inc(4)
+puts counter
+
